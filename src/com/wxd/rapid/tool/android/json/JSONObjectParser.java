6f51c3b32d9af82d2116354e5d3b904e393bc6f5
wxd56 <wxd56@126.com>
2014-01-22 12:41:10 +0800

    1221

--- src/com/wxd/rapid/tool/android/json/JSONObjectParser.java
+++ src/com/wxd/rapid/tool/android/json/JSONObjectParser.java
@@ -9,18 +9,32 @@ import org.json.JSONObject;
 import android.util.Log;
 
 /**
- * 将给定的json字符串解析成一个类（通过反射技术）
- * 
+ * 将给定的json字符串解析成一个类（通过反射技术）。<br>
+ * 支持的自动转换的类型为：String，Integer，Boolean，Long，Double。如果都不匹配则返回Object类型。
  * @author 王旭东
  *
  */
 public class JSONObjectParser {
 	
+	/**
+	 * 将给定的json字符串解析成一个类（通过反射技术）。<br>
+	 * 支持的自动转换的类型为：String，Integer，Boolean，Long，Double。如果都不匹配则返回Object类型。
+	 * 
+	 *@param jsonStr Json字符串
+	 *@param clazz   需要被转换的类型
+	 *@param return  如果参数jsonStr为空，则直接返回Null。
+	 *
+	 */
 	public static <T> T parseObject(String jsonStr, Class<T> clazz) throws Exception{
 		//得到所有属性列表
 		List<Field> props = getClassProps(clazz);
 		
 		//组装成Json对象
+		Log.d("wxd test", "解析服务器端返回的Json字符串为："+jsonStr);
+		if(jsonStr == null){
+			return null;
+		}
+			
 		JSONObject jsonObj = new JSONObject(jsonStr);
 		
 		//从Json对象中取出数据
@@ -33,8 +47,15 @@ public class JSONObjectParser {
 				value = jsonObj.getString(field.getName());
 			}else if(field.getType().isAssignableFrom(Integer.class)){
 				value = jsonObj.getInt(field.getName());
-			}
-			
+			}else if(field.getType().isAssignableFrom(Boolean.class)){
+				value = jsonObj.getBoolean(field.getName());
+			}else if(field.getType().isAssignableFrom(Long.class)){
+				value = jsonObj.getLong(field.getName());
+			}else if(field.getType().isAssignableFrom(Double.class)){
+				value = jsonObj.getDouble(field.getName());
+			}else{
+				value = jsonObj.get(field.getName());
+			}			
 			
 			if(value != null){				
 				String firstChar = field.getName().substring(0,1).toUpperCase();
